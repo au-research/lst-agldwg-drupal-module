6b9e8f1bf2336942e778413d4cbfcc4a432cbe4d
Richard Walker <Richard.Walker@ands.org.au>
2021-08-09 15:53:03 +1000

    Define filters to get emails for a comment's parent and grandparent
    Sigh, if a comment doesn't have a parent and/or a grandparent, we get
    non-expansion of tokens when trying to get an email address.
    
    Add a workaround: define filters to get the email address of a
    comment's parent and grandparent comment. If there is no
    parent/grandparent, return an empty string.

---
+++ src/Plugin/TypedDataFilter/FormatGrandparentEmailFilter.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\x1\Plugin\TypedDataFilter;
+
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\TypedData\DataDefinitionInterface;
+use Drupal\Core\TypedData\Type\StringInterface;
+use Drupal\typed_data\DataFilterBase;
+
+use Drupal\Core\Entity\TypedData\EntityDataDefinitionInterface;
+
+/**
+ * A data filter which gets the email address of a comment's grandparent,
+ * if there is one, or an empty string otherwise.
+ *
+ * @DataFilter(
+ *   id = "comment_grandparent_email",
+ *   label = @Translation("Get the email address of a comment's grandparent."),
+ * )
+ */
+class FormatGrandparentEmailFilter extends DataFilterBase {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canFilter(DataDefinitionInterface $definition) {
+        return $definition->getDataType() == 'entity:comment';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filtersTo(DataDefinitionInterface $definition,
+                              array $arguments) {
+        return DataDefinition::create('string');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filter(DataDefinitionInterface $definition, $value,
+                           array $arguments,
+                           BubbleableMetadata $bubbleable_metadata = NULL) {
+        if ($value->hasParentComment()) {
+            $pc = $value->getParentComment();
+            if ($pc->hasParentComment()) {
+                $gpc = $pc->getParentComment();
+                $gpae = $gpc->getAuthorEmail();
+                return $gpae;
+            }
+        }
+        // No grandparent comment.
+        return '';
+  }
+
+}
---
+++ src/Plugin/TypedDataFilter/FormatParentEmailFilter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\x1\Plugin\TypedDataFilter;
+
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\TypedData\DataDefinitionInterface;
+use Drupal\Core\TypedData\Type\StringInterface;
+use Drupal\typed_data\DataFilterBase;
+
+use Drupal\Core\Entity\TypedData\EntityDataDefinitionInterface;
+
+/**
+ * A data filter which gets the email address of a comment's parent,
+ * if there is one, or an empty string otherwise.
+ *
+ * @DataFilter(
+ *   id = "comment_parent_email",
+ *   label = @Translation("Get the email address of a comment's parent."),
+ * )
+ */
+class FormatParentEmailFilter extends DataFilterBase {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canFilter(DataDefinitionInterface $definition) {
+        return $definition->getDataType() == 'entity:comment';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filtersTo(DataDefinitionInterface $definition,
+                              array $arguments) {
+        return DataDefinition::create('string');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filter(DataDefinitionInterface $definition, $value,
+                           array $arguments,
+                           BubbleableMetadata $bubbleable_metadata = NULL) {
+        if ($value->hasParentComment()) {
+            $pc = $value->getParentComment();
+            $ae = $pc->getAuthorEmail();
+            return $ae;
+        } else {
+            // No parent comment.
+            return '';
+        }
+  }
+
+}
